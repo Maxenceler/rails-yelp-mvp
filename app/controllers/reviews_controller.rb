a62719fd507a6c7c71fa047b7b8627bcc3734dee
Maxence Lerouge <maxencelerouge1@gmail.com>
2021-08-11 15:49:05 +0200

    ex finished

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,27 @@
+class ReviewsController < ApplicationController
+  before_action :find_review, only: [:new, :create]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(params_review)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to @restaurant
+    else
+      render :new
+    end
+  end
+
+private
+
+  def find_review
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def params_review
+    params.require(:review).permit(:content, :rating)
+  end
+end
